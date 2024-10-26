df15dd16d10704e3d0e824492d7b4005867ec6db
gbonfoukoffivignon <gbonfoukoffivignon@gmail.com>
2024-10-26 13:33:43 +0200

    Add files via upload

---
+++ resources/views/emails/password-reset-message.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Réinitialisation de votre mot de passe</title>
+</head>
+<body>
+    <h1>Bonjour,</h1>
+    <p>Votre nouveau mot de passe est : <strong>{{ $motDePasse }}</strong></p>
+    <p>Merci de le modifier après votre connexion.</p>
+</body>
+</html>
